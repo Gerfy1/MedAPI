f6814b606481cf56d83a53abe5cdfd5653a5fd36
Gerfy <gerfynlol@gmail.com>
2024-07-04 19:53:16 -0300

    Add: create subpack: agendamento / cancelamento

--- src/main/java/med/voll/api/domain/consulta/validacoes/cancelamento/ValidadorHorarioAntecedencia.java
+++ src/main/java/med/voll/api/domain/consulta/validacoes/cancelamento/ValidadorHorarioAntecedencia.java
@@ -3,7 +3,6 @@ package med.voll.api.domain.consulta.validacoes.cancelamento;
 import med.voll.api.domain.ValidacaoException;
 import med.voll.api.domain.consulta.ConsultaRepository;
 import med.voll.api.domain.consulta.DadosCancelamentoConsulta;
-import med.voll.api.domain.consulta.validacoes.ValidadorCancelamentoDeConsulta;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
